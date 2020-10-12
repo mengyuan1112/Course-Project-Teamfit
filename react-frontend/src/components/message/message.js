70f733b3e1488a6c95bf8f53cda2fa7c5497679d
Jacob <jgoldverg@gmail.com>
2020-10-12 15:45:05 -0400

    made list based on two arrays

--- react-frontend/src/components/message/message.js
+++ react-frontend/src/components/message/message.js
@@ -4,40 +4,42 @@ import React, { Component } from 'react';
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-        position: 'relative',
-        overflow: 'auto',
-        maxHeight: 300,
-    },
-    listSection: {
-        backgroundColor: 'inherit',
-    },
-    ul: {
-        backgroundColor: 'inherit',
-        padding: 0,
-    },
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 300,
+  },
+  listSection: {
+    backgroundColor: 'inherit',
+  },
+  ul: {
+    backgroundColor: 'inherit',
+    padding: 0,
+  },
 }));
 
 export default function PinnedSubheaderList() {
-    const classes = useStyles();
-  
-    return (
-      <List className={classes.root} subheader={<li />}>
-        {[0, 1, 2, 3, 4].map((sectionId) => (
-          <li key={`section-${sectionId}`} className={classes.listSection}>
-            <ul className={classes.ul}>
-              <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-              {[0, 1, 2].map((item) => (
-                <ListItem key={`item-${sectionId}-${item}`}>
-                  <ListItemText primary={`Item ${item}`} />
-                </ListItem>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </List>
-    );
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root} subheader={<li />}>
+      {["jacobgol@buffalo.edu", "helloworld@buffalo.edu", "byeworld@buffalo.edu"].map((sectionId) => (
+        <li key={`messages-${sectionId}`} className={classes.listSection}>
+          <ul className={classes.ul}>
+            <ListSubheader>
+              {`Message: ${sectionId}`}
+            </ListSubheader>
+            {[0,1,2,3,4].map((item) => (
+              <ListItem button key={`Username-${sectionId}-${item}`}>
+                <ListItemText primary={`Message ${item}`} />
+              </ListItem>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </List>
+  );
 }
