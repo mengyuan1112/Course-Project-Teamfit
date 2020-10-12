5e1970326d9d76c545d6853049e4f417c2de12ec
mengyuan <mengyuan19971112@gmail.com>
2020-10-11 21:01:23 -0400

    adding register and login route and store in to the temporary storage, still need return stuff

---
+++ flaskr/hello.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template, redirect, url_for, flash
+import User
+
+
+app = Flask(__name__, template_folder='template', )
+
+userName_Password = {}  #temporary storage for user and password
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        user = User.User(data)
+        userName_Password[data['eMail']] = data['password']
+        print(data)
+        return "haha, succeful"#render_template()  # success register
+    if request.method == 'GET':
+        return render_template()  # reload register page
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        if data['uEmail'] in userName_Password and data['uEmail'] == userName_Password['eMail']:
+            return render_template()  # success login
+        else:
+                return render_template()  # fail login
+    if request.method == 'GET':
+        return render_template()  # reload login page
+
+
+
+if __name__ == '__main__':
+    app.run(port=3000,debug=True)
