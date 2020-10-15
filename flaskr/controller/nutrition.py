cf2f628980f761be7af1a6d33687d0e9e055413b
aidanrobbUB <61015718+aidanrobbUB@users.noreply.github.com>
2020-10-15 16:06:43 -0400

    #45 Finished taking in user input and storing it into the database

--- flaskr/controller/nutrition.py
+++ flaskr/controller/nutrition.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request
 from flask_cors import CORS
 from datetime import date
+import psycopg2
+import json
 
 app = Flask(__name__, template_folder='template', )
 
@@ -19,7 +21,38 @@ def nutritionSubmit():
         dateformat = today.strftime("%m/%d/%y")
         calories = ((9 * int(fat)) + (4 * int(protein)) + (4 * int(carbs)))
         # Save this info to user  in database
-    return "Page for handling nutrition"
+        email = "test@test.com"
+
+        conn = psycopg2.connect(
+            database='teamfit',
+            user='aidan',
+            password='roach',
+            sslmode='require',
+            sslrootcert='certs/ca.crt',
+            sslkey='certs/client.aidan.key',
+            sslcert='certs/client.aidan.crt',
+            port=26257,
+            host='localhost'
+        )
+
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM teamfit.nutrition")
+            row = cur.fetchall()
+            for i in range(len(row)):
+                if email in row[i]:
+                    idXtra, nutHistory = row[i]
+                    data = json.dumps(nutHistory)
+                    jsonData = json.loads(data)
+                    jsonData['date'].append(dateformat)
+                    jsonData['calories'].append(calories)
+                    jsonData['weight'].append(weight)
+                    finishedData = json.dumps(jsonData)
+                sql = 'INSERT INTO teamfit.nutrition (id, history) VALUES (%s,%s)'
+                val = (email, finishedData)
+                cur.execute(sql, val)
+        conn.commit()
+
+    return "This is for processing"
 
 
 if __name__ == '__main__':
