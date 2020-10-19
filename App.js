dbf2949e848b7afa7df8ae1a68c26ce0b4e23cea
whransom <70912451+whransom@users.noreply.github.com>
2020-10-19 23:41:48 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+import Home from './components/homepage/home.js'
+import NavBar from './components/navBar/navBar';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+//import { Paper, Tabs } from '@material-ui/core';
+import './App.css';
+import Login from './components/logReg/login.js';
+import Register from './components/logReg/register.js'
+import Profile from './components/profile/profile.js';
+// import friends from './components/friends/friends.js';
+import Friends from './components/Friends/Friends.js';
+import Feed from './components/post/feed.js';
+
+
+/**
+ * Root Component
+ * -contains child component that swaps out to the other components.
+ */
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name: "root",
+      showNavBar: true,
+      showProfile: false,
+      showLogin: false,
+      showHome: false,
+      showRegister: false
+    }
+  }
+  state = {
+    currentPage: 'compApp'
+  }
+
+  hideComponent(name){
+    console.log(name);
+    switch (name) {
+      case "home":
+        this.setState({showHome: !this.state.showHome});
+        break;
+      case "Friends":
+          this.setState({shofriends: !this.state.showfriends});
+          break;
+      case "profile" :
+        this.setState({showProfile: !this.state.showProfile});
+        break;
+        case "register" :
+        this.setState({showRegister: !this.state.showRegister});
+        break;
+      default:
+        this.setState({showLogin: !this.state.showLogin}); 
+    }
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    return(
+      <BrowserRouter basename="/SnapScout">
+      <div className="container">
+        <Route exact path="" render={() => <NavBar />}/>
+        <Switch>
+          <Route exact path="/home" render={() => <Home />}/>
+          <Route path="/friends" render={() => <Friends />} />
+          <Route path="/login" render={() => <Login />} />
+          <Route path="/profile" render={() => <Profile />} />
+          <Route path="/register" render={() => <Register />} /> 
+        </Switch>
+      </div>
+    </BrowserRouter>
+    )};
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
+
+export default App;
