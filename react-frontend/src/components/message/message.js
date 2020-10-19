ca54c644680f947f632c495d663a1957e27daf70
Jacob <jgoldverg@gmail.com>
2020-10-19 15:32:58 -0400

    added form

--- react-frontend/src/components/message/message.js
+++ react-frontend/src/components/message/message.js
@@ -1,4 +1,4 @@
-import { List, ListItem, ListItemText, ListSubheader, TextField, Button, RaisedButton } from '@material-ui/core';
+import { List, ListItem, ListItemText, ListSubheader, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React, { useState } from 'react';
 const axios = require('axios');
@@ -8,9 +8,9 @@ export default class Messages extends React.Component {
     super(props);
     this.state = {
       sourceEmail: "jacobgol@buffalo.edu",
-      destEmail: null,
-      header: null,
-      messageList: [] ,
+      destEmail: "",
+      header: "",
+      messageList: [],
       parentCounter: 0,
       formErrors: {
         sourceEmail: "",
@@ -20,57 +20,68 @@ export default class Messages extends React.Component {
     }
   }
 
-  listMessages(){
-    axios.get('http://localhost:5000/listMessages', {headers: {"userID":this.state.sourceEmail}})
-    .then(function(data){
-      this.state.messageList.push(data)
-    })
+  listMessages() {
+    axios.get('http://localhost:5000/listMessages', { headers: { "userID": this.state.sourceEmail } })
+      .then(function (data) {
+        this.state.messageList.push(data)
+      })
   }
 
-  listParents(){
-    axios.get('http://localhost:5000/listParentMessages', {headers: {"parentID":this.state.sourceEmail}})
-    .then(function(data){
-      this.state.parentList.push(data)
-    })
+  listParents() {
+    axios.get('http://localhost:5000/listParentMessages', { headers: { "parentID": this.state.sourceEmail } })
+      .then(function (data) {
+        this.state.parentList.push(data)
+      })
   }
-//handles form button being clicked.
-  handleSubmit(event){
-    axios.post('/createMessage', {
-//      header: this.state.header,
-      parentMessageID: this.state.parentMessageID,
+  //handles form button being clicked.
+  createBody = (n) => {
+     return {
+      header: this.state.header,
+      parentMessageID: this.state.parentCounter,
       userID: this.state.sourceEmail,
       recieverID: this.state.recieverID,
-      content: this.state.content
-    }).then(function(data){
-      console.log('submitted ur message')
-    })
+      content: this.state.content  
+    }
+
+  }
+  handleSubmit(event) {
+    fetch('http://localhost:5000/createMessage', {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(this.createBody)
+    }).then(response => response.json())
+    window.location.reload()
   }
 
-  updateUserId(event){
-    this.setState({sourceEmail: event.target.value})
+  updateSourceEmail(event) {
+    this.setState({ sourceEmail: event.target.value })
   }
 
-  updateDestEmail(event){
-    this.setState({destEmail: event.target.value})
+  updateDestEmail(event) {
+    this.setState({ destEmail: event.target.value })
   }
 
-  updateParentID(event){
-    this.setState({parentID: ++parentCounter})
+  updateParentID(event) {
+    this.setState({ parentCounter: ++this.state.parentCounter })
   }
 
-  updateHeader(event){
-    this.setState({header: event.target.value})
+  updateHeader(event) {
+    this.setState({ header: event.target.value })
   }
 
-  updateContent(event){
-    this.setState({content: event.target.value})
+  updateContent(event) {
+    this.setState({ content: event.target.value })
   }
 
   componentDidMount() {
 
   }
 
-  render(){
+  render() {
     const classesList = makeStyles((theme) => ({
       root: {
         width: '100%',
@@ -99,33 +110,33 @@ export default class Messages extends React.Component {
     return (
       <div>
         <div>
-        <form onSubmit={this.handleSubmit}>
-          <TextField error id="standard-error" label="Error" defaultValue="your@gmail.com" onChange={this.updateUserId}/>  
-          <TextField error id="standard-error" label="Error" defaultValue="recieverEmail@gmail.com" onChange={this.updateDestEmail} />
-          <TextField errir id="standard-error" label="Error" defaultValue="Enter your message header" onChange={this.updateHeader}>      
-          <TextField errir id="standard-error" label="Error" defaultValue="Enter your message" onChange={this.updateContent}>      
-          <RaisedButton type="submit" label="login" className="button-submit" primary={true} onClick={this.handleSubmit}/>
-        </form>
+          <form onSubmit={this.handleSubmit}>
+            <TextField error id="standard-error" label="Error" defaultValue="your@gmail.com" onChange={this.updateSourceEmail} />
+            <TextField error id="standard-error" label="Error" defaultValue="recieverEmail@gmail.com" onChange={this.updateDestEmail} />
+            <TextField defaultValue="Enter message header here" onChange={this.updateHeader} />
+            <TextField defaultValue="Enter message here" onChange={this.updateContent} />
+            <Button label="Send" variant="contained" className="button-submit" color="primary">Send</Button>
+          </form>
         </div>
-      <div>
-      <List className={classesList.root} subheader={<li />}>
-        {["jacobgol@buffalo.edu", "helloworld@buffalo.edu", "byeworld@buffalo.edu"].map((sectionId) => (
-          <li key={`messages-${sectionId}`} className={classesList.listSection}>
-            <ul className={classesList.ul}>
-              <ListSubheader>
-                {`Message: ${sectionId}`}
-              </ListSubheader>
-              {[0,1,2,3,4].map((item) => (
-                <ListItem button key={`Username-${sectionId}-${item}`}>
-                  <ListItemText primary={`Message ${item}`} />
-                </ListItem>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </List>
+        <div>
+          <List className={classesList.root} subheader={<li />}>
+            {["jacobgol@buffalo.edu", "helloworld@buffalo.edu", "byeworld@buffalo.edu"].map((sectionId) => (
+              <li key={`messages-${sectionId}`} className={classesList.listSection}>
+                <ul className={classesList.ul}>
+                  <ListSubheader>
+                    {`Message: ${sectionId}`}
+                  </ListSubheader>
+                  {[0, 1, 2, 3, 4].map((item) => (
+                    <ListItem button key={`Username-${sectionId}-${item}`}>
+                      <ListItemText primary={`Message ${item}`} />
+                    </ListItem>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </List>
+        </div>
       </div>
-      </div>     
-    );  
+    );
   }
 }
