81741ebd23302e59846b2db4af4ac71149023046
Mohammad Islam <mhislam@buffalo,edu>
2020-10-19 13:24:40 -0400

    Working on frontend and run issue

--- react-frontend/src/components/homepage/home.js
+++ react-frontend/src/components/homepage/home.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
+// import Newsfeed from './../newsfeed.js';
+import Axios from "axios";
 
 class Home extends Component{
+    state = {
+        articles: []
+      }
 
     componentDidMount(){
-
+        Axios.get('http://127.0.0.1:5000/home')
+        .then(response => {
+            const articles = response.data
+            this.setState({articles})
+            }); 
     }
 
     componentWillUnmount(){
-        
+
     }
 
     render() {
         return (
         <div>
             <h1>Welcome to the homepage</h1>
+            {this.state.articles.map(response => {
+                return <li>{response.title}</li>;
+            })}
         </div>
         )};
 }
 
-export default Home
+export default Home;
