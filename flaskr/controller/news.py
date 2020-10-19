cca2bcf81ac330cefbda95eb9031082b0053d93b
Mohammad Islam <mhislam@buffalo,edu>
2020-10-18 22:30:29 -0400

    Wrote python backend for request to NewsAPI and created new component for the frontend

---
+++ flaskr/controller/news.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify, render_template
+# from model import News # figure out how to fix this for the news article_info fields
+from newsapi import NewsApiClient
+import json
+
+app = Flask(__name__, template_folder='public',)
+
+
+
+@app.route("/home", methods=['GET'])
+def get_news():
+    
+    # with app.app_context():
+        newsapi = NewsApiClient(api_key = '29b0d1fda8b6452fb4df7d86a3dc5b9a')
+        data = newsapi.get_everything(q='health and fitness', language='en', page_size=20)
+        articles = data['articles']
+        # print(type(articles[0])) It showed that it is of dict type.
+        articles_json = json.dumps(articles)
+        return articles_json
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
+
+
+
+
+
+
+
+
+    # source = []  
+    # title = []
+    # author = []
+    # link = []
+    # image = []
+    # date = []
+    # content = []
+    # print(type(articles))
+    # print(type(articles[0]))
+    # print(articles[0])
+    # for entry in range(len(articles)):
+    #     news_entry = articles[entry] 
+    #     source.append(news_entry['source']['name']) 
+    #     title.append(news_entry['title'])
+    #     author.append(news_entry['author'])
+    #     link.append(news_entry['url'])
+    #     image.append(news_entry['urlToImage'])
+    #     date.append(news_entry['publishedAt'])
+    #     content.append(news_entry['content'])
+    # print(source[0])
+    # combination = zip(source,title,author,link, image, date, content)
+    # return combination 
+
+# This is how to fetch the source from the JSON
+# articles = data['articles'] 
+# source = articles[0] 
+# print(source['source']['name'])
+
+# Thw following line also accomplishes the same thing
+# print(data['articles'][0]['source']['name'])
+# print(get_news())
---
+++ react-frontend/src/components/news/newsfeed.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Axios from "axios";
+// import Post from './post.js';
+
+// import './feed.css';
+// import './post.css';
+
+class Newsfeed extends Component {
+    constructor(props) {
+        super();
+
+    }
+
+    componentDidMount() {
+        
+    }
+}
