5cd1ede5fa62c090fa4b4121b5fbb61f0ce068fd
mengyuan <mengyuan19971112@gmail.com>
2020-10-27 21:55:53 -0400

    //update register/LoginDB info,

--- flaskr/controller/logReg.py
+++ flaskr/controller/logReg.py
@@ -82,15 +82,15 @@ def login():
             host='localhost',
             sslmode='disable'
         )
-        with conn.cursor as cur:
-            cur.execute("SELECT * FROM teamfit.user")
-            row = cur.fetchall
+        with conn.cursor() as cur:
+            cur.execute("SELECT * from teamfit.user")
+            row = cur.fetchall()
             for i in row:
-                if i[0] == int(user_number) and i[1] == use_password:
+                if i[0] == user_number and i[1] == use_password:
                     cur.close()
                     conn.close()
                     return jsonify({'state': "Successful login"}), 200
-                elif i[0] == int(user_number) and i[1] != use_password:
+                elif i[0] == user_number and i[1] != use_password:
                     cur.close()
                     conn.close()
                     return jsonify({'state': "Password wrong"}), 200
