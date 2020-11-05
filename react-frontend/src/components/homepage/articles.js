8f57accf88038e135bcd4a1231bd04d202f5f995
Zayed Ahmed <zahmed@verizonmedia.com>
2020-11-05 12:11:05 -0500

    added the news feed to home page + logo

---
+++ react-frontend/src/components/homepage/articles.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import Axios from "axios";
+import './home.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Card from 'react-bootstrap/Card'
+
+var today = new Date(),
+date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+const APIHealth = "http://newsapi.org/v2/everything?q=diet&from={date}&sortBy=popularity&apiKey=ad4f58957a6041f4b408ef2d94330a31"
+const APIFitness = "http://newsapi.org/v2/everything?q=fitness&from={date}&sortBy=popularity&apiKey=ad4f58957a6041f4b408ef2d94330a31"
+
+
+
+
+class Articles extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch(APIFitness)
+         .then(response => response.json())
+         .then(data => this.setState({ articles: data.articles }));
+    }
+
+    
+    render() {
+        const {articles} = this.state;
+        return (
+            <>
+            <aside style={{marginLeft: "5%",display: "inline-block"}}>
+                <h2>News Feed</h2>
+                {articles.map(article =>
+                    <Card style={{width: '50rem'}}>
+                        <Card.Header></Card.Header>
+                        <Card.Body>
+                             <aside style={{display: 'inline-block'}}><img src={article.urlToImage} width="180" height="180" alt="ArticleImage"/></aside>
+                             <div style={{display: 'inline-block'}}>
+                            <Card.Title ><a href={article.url}>{article.title}</a></Card.Title>
+                            <Card.Subtitle clasnName="mb-2 text-muted">{article.source.name}</Card.Subtitle>
+                            <Card.Text>{article.description}</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                    )}
+            </aside>
+            </>
+        )
+    };
+}
+
+export default Articles
