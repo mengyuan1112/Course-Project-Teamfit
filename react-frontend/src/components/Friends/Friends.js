dfa1b3a5f15c9a8f805fe0d5d795ae8dcfc589a8
whransom <70912451+whransom@users.noreply.github.com>
2020-11-08 07:14:18 -0800

    Add the spotters and suggested column

--- react-frontend/src/components/Friends/Friends.js
+++ react-frontend/src/components/Friends/Friends.js
@@ -4,32 +4,77 @@ import '../post/feed'
 import avatar from "../../avatar.jpg";
 //import './Friends.css'
 import Feed from '../post/feed';
-
+import Masonry from 'react-masonry-css'
+import Axios from 'axios';
+import table from 'react-bootstrap/table';
+import Button from 'react-bootstrap/Button';
 class friends extends React.Component{
 
-    componentDidMount(){
-
+    componentDidMount() {
+        Axios.get('http://localhost:5000/friends/friends_get').then(response=> { this.setState({userInfo: response.data});})
+        console.log(this.state.userInfo)
     }
 
     componentWillUnmount(){
         
     }
+    constructor(props) {
+        super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+        this.state = { //state is by default an object
+           students: [
+              {   Spotters: 'Hank',Suggested: 'ALI' },
+              {   Spotters: 'Michel', Suggested: 'JOE' },
+              {   Spotters: 'Syed', Suggested: 'JACKIE' },
+              {   Spotters: 'Asad', Suggested: 'WILLIAM' }
+           ]
+        }
+     }
+     renderTableHeader() {
+        let header = Object.keys(this.state.students[0])
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+     }
+
+     renderTableData() {
+        return this.state.students.map((student, index) => {
+           const { Spotters , Suggested } = student //destructuring
+           return (
+              <tr >
+                
+                 <td>{Spotters}</td>
+                 {/* <td>{"     "}</td> */}
+                
+                 {/* <td>{"     "}</td> */}
+           <td>{" "}{Suggested}</td>
+              </tr>
+           )
+        })
+     }
+  
 
     render() {
         return (
-        <div>
-            <div className="rows">
-                <div className="row">
-                    <h1>Friends:</h1>
-                    <img src={avatar} width="180" height="180" alt="Login Image" />
-                    <img src={avatar} width="180" height="180" alt="Login Image" />
-                </div> 
+            <div >
+               
+                {/* <table cellpadding="10" cellspacing="10"></table> */}
+               <h1 id='title'>Friends Page</h1>
+               {/* <td>Spotters</td>
+                 
+        <td>{" "}Suggested</td> */}
+               <table cellpadding="10" cellspacing="8" id='students' >
+                  <tbody>
+                  
+                     <tr cellpadding="10" cellspacing="10">{this.renderTableHeader()}</tr>
+                     {this.renderTableData()}
+                  </tbody>
+               </table>
             </div>
-        </div>
-        )
+            
+         )
     };
 }
 
 
-
+// ReactDOM.render(<friends />, document.getElementById('root'));
 export default friends;
