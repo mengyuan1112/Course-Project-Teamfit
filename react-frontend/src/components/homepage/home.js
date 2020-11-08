306342a3c5f7d8801e030763e25b27ece6f5753a
mengyuan <mengyuan19971112@gmail.com>
2020-11-08 01:55:51 -0500

    add search Id button add search recipe button make card scale larger

--- react-frontend/src/components/homepage/home.js
+++ react-frontend/src/components/homepage/home.js
@@ -17,77 +17,14 @@ class Home extends Component{
         super(prop);
         this.state = {
             foodInfo: [],
-            calories: "",
-            caloriesVal: "",
-            caloriesRangeVal:[],
-            caloriesRange:"",
-            fat:"",
-            fatVal:"",
-            fatRangeVal:[],
-            fatRange:"",
-            carbs: "",
-            carbsVal:"",
-            carbsRangeVal:[],
-            carbsRange:"",
-            protein: "",
-            proteinVal: "",
-            proteinRange: "",
-            proteinRangeVal:[],
+            foodNutrition:[],
             foodName: "",
             message: "",
+            foodId: [],
+            recipeInfo: [],
             articles: []
+
         }
-    }
-
-    handleShows = e =>{
-        Axios.get('http://127.0.0.1:5000/home')
-            .then(response => {
-                const articles = response.data
-                this.setState({
-                    foodInfo: [],
-                    calories: "",
-                    caloriesVal: "",
-                    caloriesRangeVal:[],
-                    caloriesRange:"",
-                    fat:"",
-                    fatVal:"",
-                    fatRangeVal:[],
-                    fatRange:"",
-                    carbs: "",
-                    carbsVal:"",
-                    carbsRangeVal:[],
-                    carbsRange:"",
-                    protein: "",
-                    proteinVal: "",
-                    proteinRange: "",
-                    proteinRangeVal:[],
-                    foodName: "",
-                    message: "",
-                    articles: articles})
-            });
-    }
-
-    handleUnshow=e=>{
-        this.setState({foodInfo: [],
-            calories: "",
-            caloriesVal: "",
-            caloriesRangeVal:[],
-            caloriesRange:"",
-            fat:"",
-            fatVal:"",
-            fatRangeVal:[],
-            fatRange:"",
-            carbs: "",
-            carbsVal:"",
-            carbsRangeVal:[],
-            carbsRange:"",
-            protein: "",
-            proteinVal: "",
-            proteinRange: "",
-            proteinRangeVal:[],
-            foodName: "",
-            message: "",
-            articles: []})
     }
 
     handleChange = e => {
@@ -110,55 +47,85 @@ class Home extends Component{
                 let res = response.data;
                 console.log(res)
                 if(res['state'] == 'Not enough data for an informed guess.'){
-                    this.setState({foodInfo: [],
-                        calories: "",
-                        caloriesVal: "",
-                        caloriesRangeVal:[],
-                        caloriesRange:"",
-                        fat:"",
-                        fatVal:"",
-                        fatRangeVal:[],
-                        fatRange:"",
-                        carbs: "",
-                        carbsVal:"",
-                        carbsRangeVal:[],
-                        carbsRange:"",
-                        protein: "",
-                        proteinVal: "",
-                        proteinRange: "",
-                        proteinRangeVal:[],
-                        foodName: "",
+                    this.setState({
+                        foodInfo: [],
                         message: res['state'],
+                        foodId: [],
+                        foodNutrition: [],
                         articles: []})
                 }
                 else this.setState(
                     {
                         message: "",
-                        calories: res['state'][0],
-                        caloriesVal:res['state'][1],
-                        caloriesRange: res['state'][2],
-                        caloriesRangeVal:res['state'][3],
-                        protein:res['state'][4],
-                        proteinVal:res['state'][5],
-                        proteinRange:res['state'][6],
-                        proteinRangeVal:res['state'][7],
-                        fat:res['state'][8],
-                        fatVal:res['state'][9],
-                        fatRange:res['state'][10],
-                        fatRangeVal:res['state'][11],
-                        carbs:res['state'][12],
-                        carbsVal:res['state'][13],
-                        carbsRange:res['state'][14],
-                        carbsRangeVal:res['state'][15],
+                        foodNutrition: res['state'],
+                        foodId: [],
                         articles: []}
                         )
             })
     }
 
+    handleSubmitId = e =>{
+        e.preventDefault()
+        axiosConfig.post('http://127.0.0.1:5000/home/foodID',{
+            body : this.state.foodName,
+            headers:{"Content-Type":"application/json",},
+            cache: "no-cache",
+        }).then(
+            response => {
+                let res = response.data
+                this.setState({
+                    foodInfo: [],
+                    foodNutrition:[],
+                    message: "",
+                    foodId: res['state'],
+                })
+                console.log(this.state.foodId);
+            }
+        )
+    }
+
+    handleSubmitRecipe =e=>{
+        e.preventDefault()
+        axiosConfig.post('http://127.0.0.1:5000/home/foodRecipe',{
+            body : this.state.foodName,
+            headers:{"Content-Type":"application/json",},
+            cache: "no-cache",
+        }).then(
+            response => {
+                let res = response.data
+                this.setState({
+                    foodInfo: [],
+                    // calories: "",
+                    // caloriesVal: "",
+                    // caloriesRangeVal:[],
+                    // caloriesRange:"",
+                    // fat:"",
+                    // fatVal:"",
+                    // fatRangeVal:[],
+                    // fatRange:"",
+                    // carbs: "",
+                    // carbsVal:"",
+                    // carbsRangeVal:[],
+                    // carbsRange:"",
+                    // protein: "",
+                    // proteinVal: "",
+                    // proteinRange: "",
+                    // proteinRangeVal:[],
+                    foodNutrition:[],
+                    foodName: "",
+                    message: "",
+                    foodId: [],
+                    recipeInfo: res['state'],
+                    articles: []
+                })
+            }
+        )
+    }
 
     componentWillUnmount(){
 
     }
+
 
     render() {
         return (
@@ -166,7 +133,7 @@ class Home extends Component{
             <img src={loginImg} width="180" height="180" alt="LoginImage" className="logo"/>
             
 
-                <div style={{height: "180px", marginTop: "5%", marginLeft: "5%"}}>
+                <div style={{height: "250px", marginTop: "5%", marginLeft: "5%"}}>
 
                 {/*searchBar for show*/}
                 <h2>Nutrition Info:</h2>
@@ -176,8 +143,9 @@ class Home extends Component{
                     onChange={this.handleChange}
                 />
                 {/*search button*/}           
-                <button onClick={this.handleSubmit} className="foodSearch">Search Food!</button>
-
+                    <button onClick={this.handleSubmit} className="foodSearch">Search Food!</button>
+                    <button onClick={this.handleSubmitId} className = "foodSearchID"> Search Food ID!</button>
+                    <button onClick={this.handleSubmitRecipe} className = "foodSearchRecipe"> Food Recipe By ID!</button>
 
                 {/*display the context*/}
                     {this.state.articles.map(response => {
@@ -185,18 +153,16 @@ class Home extends Component{
                         })}
                     <Card style={{width: '50rem'}}>
                         <Card.Body>
-                        {this.state.articles.map(response => {
-                        return <li>{response.title}</li>;
+                        {this.state.foodId.map(response=>{
+                            return <body>{response}</body>;
                         })}
-                        { this.state.message}<br/>
-                        { this.state.calories + this.state.caloriesVal}<br/>
-                        { this.state.caloriesRange + this.state.caloriesRangeVal}<br/>
-                        { this.state.protein + this.state.proteinVal}<br/>
-                        { this.state.proteinRange + this.state.proteinRangeVal}<br/>
-                        { this.state.fat +this.state.fatVal}<br/>
-                        { this.state.fatRange+ this.state.fatRangeVal}<br/>
-                        { this.state.carbs + this.state.carbsVal}<br/>
-                        { this.state.carbsRange + this.state.carbsRangeVal}
+                        {this.state.recipeInfo.map(response=>{
+                            return <body>{response}</body>;
+                        })}
+                        {this.state.foodNutrition.map(response=>{
+                            return <body>{response}</body>
+                        })}
+                        { this.state.message}
                         </Card.Body>
                     </Card>
                     
