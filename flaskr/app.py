216c4f48cae935dac906c4d095c8a8482f38a43c
mengyuan <mengyuan19971112@gmail.com>
2020-11-30 00:18:23 -0500

    Merge branch 'development' of https://github.com/UB-CSE/course-project-teamfit into development
    # Conflicts:
    #	react-frontend/package.json

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,11 +2,12 @@ from flask import (Flask)
 from flask_cors import CORS
 from .controller.nutrition import nutrition_page
 from .controller.logReg import logReg_page
-from .controller.message import message_page
+# from .controller.message import message_page
 from .controller.foodSearch import foodSearch_page
 from .controller.news import news_page
 from .controller.profile import profile_page
 from .controller.fitness import fitness_page
+from .controller.caloriesHistory import caloriesHistory_page
 
 app = Flask("__main__")
 app.register_blueprint(nutrition_page)
@@ -17,6 +18,7 @@ app.register_blueprint(news_page)
 app.register_blueprint(profile_page)
 app.register_blueprint(news_page)
 app.register_blueprint(fitness_page)
+app.register_blueprint(caloriesHistory_page)
 
 
 CORS(app)
