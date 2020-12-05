581fd581a817ec0c9d74cc330e404f0c7ba7ef5a
whransom <70912451+whransom@users.noreply.github.com>
2020-12-05 08:44:59 +0500

    Add files via upload
    Suggested friends backend and styling page

--- react-frontend/src/components/Friends/Friends.js
+++ react-frontend/src/components/Friends/Friends.js
@@ -1,11 +1,59 @@
-import React from 'react';
+// import React from 'react';
 import '../post/feed'
 import './Friends.css'
 import axiosConfig from 'axios';
 import Axios from 'axios';
+import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css'
+import "./App.css";
+
+const Person = ({ img, name, occupation, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <div className="person">
+      <img src={url} alt="person img" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{occupation}</h4>
+        {children}
+      </div>
+    </div>
+  );
+};
+const PersonList = () => {
+   
+   
+  return (
+   
+    <section className="person-list" >
+        <h1 className="person-list">Suggested Friends:</h1>
+      <Person img="34" name="john" >
+      <button class="mini ui button"  >
+  <i class="icon user"></i>
+  Add Friend
+</button>
+         </Person>
+      <Person img="22" name="bob" >
+      
+      <button class="mini ui button" >
+  <i class="icon user"></i>
+  Add Friend
+</button>
+
+      </Person>
+      <Person img="56" name="david"  >
+      <button class="mini ui button">
+  <i class="icon user"></i>
+  Add Friend
+</button>
+         </Person>
+    </section>
+  );
+};
 
 class friends extends React.Component{
 
+  
     constructor(props) {
         super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
         this.state = { //state is by default an object
@@ -54,10 +102,17 @@ class friends extends React.Component{
       return (
             <div >               
                <h2 className='title'>Friends:</h2>
+               
+               
+               <PersonList />;
+               
+               
+               
                {Object.keys(folks).map((key,id) => {
                   return <h2 key={id} className="friend">{this.state.friends[key]} | {key}</h2>
                })}
-            <form onSubmit={this.handleSubmit}>
+              
+              <form onSubmit={this.handleSubmit}>
             <label style={{ fontWeight: 'bold' }}>
                Add a friend: 
                <input type="text" value={this.state.friendNum} onChange={this.handleChange}/>
@@ -66,7 +121,13 @@ class friends extends React.Component{
             </form>  
             </div>      
          )
+         
+            
+          
+         
     };
+    
+     
 }
 
 
